e1a39725418cad29d96d9d367243dadfcf6ed000
Nakul Jadhav <jadhavnakul456@gmail.com>
2026-06-22 12:55:18 +0530

    feat: implement CRUD operations for todo items in FastAPI app

--- CRUD/app.py
+++ CRUD/app.py
@@ -3,13 +3,31 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-
+todos=[]
 
 class Todo(BaseModel):
     id:int
     title:str
     completed:bool
 
-@app.post("/todo")
+# Create
+
+@app.post("/todos")
 def create_todo(todo:Todo):
-    return 
+    todos.append(todo)
+    return{"message":"TODO added",'data':todo}
+
+# Read
+
+@app.get("/todos")
+def get_todos():
+     return todos
+
+# Reading single data using path params
+
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id:int):
+    for todo in todos:
+        if todo.id==todo_id:
+            return todo
+    return{"error":"Todo not found"}
