157558a1841af8bbc04ac680414f08dab42971fe
Nakul Jadhav <jadhavnakul456@gmail.com>
2026-06-23 11:50:25 +0530

    feat: implement global exception handling and custom status codes for user endpoints

--- status_code_responses/app.py
+++ status_code_responses/app.py
@@ -1,7 +1,42 @@
-from fastapi import FastAPI,status,HTTPException
+'''
+Custom Exception is userdefined exception which is used in specific buisness logic error   
+HTTPException is built in FastApi ,to handle custom exception 
+Global Exception Handler is used to handle the exception in the entire application it can also raise the HTTPException.
+---> benifits of Global Exception Handler 
+    1. DRY (Don't Repeat Yourself)
+    2. Consistent Error Handling
+    3. Centralized Error Management
+    4. Better User Experience
+    5. Scalabel API design
+'''
+
+
+from pydantic_models.app import User
+from fastapi import FastAPI,status,HTTPException,Request
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 app = FastAPI()
+
+
+# Global erorr Handler 
+
+class UserNotFoundException(Exception):
+    def __init__(self,name:str):
+        self.name = name
+
+@app.exception_handler(UserNotFoundException)
+def user_not_found_exception_handler(request:Request,exc:UserNotFoundException):
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"status":"error","message":f"User {exc.name} Not Found"}
+    )
+
+@app.get("/user/{name}")
+def get_user(name:str):
+    if name!="Mohit":
+        raise UserNotFoundException(name)
+    return{"name":name}
 
 # Custom status code
 
@@ -30,7 +65,16 @@ def get_user():
 
 @app.get("/user/{id}")
 def get_user(id:int):
-    if id==5:
-        return {"message":"User Found"}
-    else:
+    if id!=1:
         raise HTTPException(status_code=404,detail="User Not Found")
+        
+    return{
+        "id":1,
+        "name":"Mohit",
+        "age":22,
+        "email":"[EMAIL_ADDRESS]"
+    }
+
+    # Custom excpetion Error Handling
+
+
